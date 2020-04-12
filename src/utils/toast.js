be34fa5550e000da17290932c45fb3b1b44090a5
pratishshr <pratishshr@gmail.com>
2020-04-12 13:07:34 +0545

    Add a toast utility

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -4,4 +4,4 @@ export function success({ title, message }) {}
 
 export function error({ title, message }) {}
 
-export function warning({ title = '', message }) {}
+export function warning({ title, message }) {}
