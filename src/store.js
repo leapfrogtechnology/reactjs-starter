5baa8f2e923fcb62e119f976d3a059119ee218fc
pratishshr <pratishshr@gmail.com>
2020-04-18 14:03:49 +0545

    Add constants for environment

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import thunk from 'redux-thunk';
 import { compose, createStore, applyMiddleware } from 'redux';
 
+import * as env from 'constants/env';
+
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    process.env.REACT_APP_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__
+    process.env.REACT_APP_ENV !== env.PRODUCTION && window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f
   )
