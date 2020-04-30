fce461d96da0db2484cd09cf8ca6a314c0900f11
bishal.j.rayamajhi@gmail.com <bishal.j.rayamajhi@gmail.com>
2020-04-30 13:23:21 +0545

    Update scss starter kit from new UI kit

--- mock-api/user.js
+++ mock-api/user.js
@@ -8,6 +8,6 @@ module.exports = (req, res, next) => {
       },
     });
   } else {
-    next()
+    next();
   }
 };
--- src/schemas/EmployeeSchema.js
+++ src/schemas/EmployeeSchema.js
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 const EmployeeSchema = Yup.object().shape({
   firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
   lastName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-  designation: Yup.string().min(2,'Must be 2 characters or more').required('Required')
+  designation: Yup.string().min(2, 'Must be 2 characters or more').required('Required'),
 });
 
 export default EmployeeSchema;
