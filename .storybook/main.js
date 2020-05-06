f0b24e13745d18dbf9287c05a4a65ef16816964a
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-05-06 12:18:31 +0545

    Remove unused import in storybook main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links'],
