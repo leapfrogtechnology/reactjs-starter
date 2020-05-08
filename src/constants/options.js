23ca9e7a44851e84c4154aeacf9cf045518ada46
Samir Oli <samiroli789@gmail.com>
2020-05-08 13:29:52 +0545

    resolved  PR#32 merge conflicts

--- src/constants/options.js
+++ src/constants/options.js
@@ -2,3 +2,12 @@ export const TYPE_OPTIONS = ['TYPE1', 'TYPE2', 'TYPE3'];
 
 export const ASC = 'asc';
 export const DESC = 'desc';
+
+export const DESIGNATION_OPTIONS = [
+  'Associate Software Enginner',
+  'Finance Officer',
+  'QA Engineer',
+  'Senior Software Engineer',
+  'Software Engineer',
+  'Project Manager',
+];
